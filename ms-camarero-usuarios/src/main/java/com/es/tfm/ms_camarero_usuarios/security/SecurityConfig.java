73815f23490a0cc46b03f2943cca05df16601316
karataker <luisosle@hotmail.com>
2025-05-13 22:57:28 +0200

    [LOG] Fix Csrf

--- ms-camarero-usuarios/src/main/java/com/es/tfm/ms_camarero_usuarios/security/SecurityConfig.java
+++ ms-camarero-usuarios/src/main/java/com/es/tfm/ms_camarero_usuarios/security/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()

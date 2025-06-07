5d73fb1130a6536e72e9b37c7b320f3e3975d04b
Jose Maria Blasco Roman <missicullera@gmail.com>
2025-06-07 13:58:49 +0200

    nombre del bar en la carta

--- ms-camarero-reservas/src/main/java/com/es/tfm/ms_camarero_reservas/controller/BarController.java
+++ ms-camarero-reservas/src/main/java/com/es/tfm/ms_camarero_reservas/controller/BarController.java
@@ -2,9 +2,12 @@ package com.es.tfm.ms_camarero_reservas.controller;
 
 import com.es.tfm.ms_camarero_reservas.model.Bar;
 import com.es.tfm.ms_camarero_reservas.repository.BarRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/bares") // This remains the same as it's for top-level bar operations
@@ -19,5 +22,15 @@ public class BarController {
     @GetMapping
     public List<Bar> getAllBares() {
         return barRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Bar> getBarById(@PathVariable Long id) {
+        Optional<Bar> barOptional = barRepository.findById(id);
+        if (barOptional.isPresent()) {
+            return ResponseEntity.ok(barOptional.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
